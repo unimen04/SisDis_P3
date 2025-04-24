e94edc961f54b6b816ca3b8034af361de169b26f
unimen04 <unai.mendoza@estudiante.uam.es>
2025-04-24 09:45:52 +0200

    funchoza

--- pages/tareas.vue
+++ pages/tareas.vue
@@ -1,32 +1,21 @@
 <template>
     <v-container>
-        <v-text-field label="" v-model="nuevaTarea"></v-text-field>
+        <v-row class="align center">
+            <v-col>
+                <h1>Tareas</h1>
+            </v-col>
 
-        <v-btn color="primary" @click="agregarTarea">Agregar Tarea</v-btn>
+        <v-btn color="primary" @click="agregar">
+            Agregar tarea
+        </v-btn>
 
-        <v-data-table :items="items" :headers="headers"></v-data-table>
-
+        </v-row>
     </v-container>
 </template>
-    
-    <script setup>
-    const headers = [
-        { text: 'Name', value: 'name' },
-        { text: 'Completada', value: 'completada' },
-    ]
-    
-    items = [
-        { name: 'Tarea 1', completada: true },
-        { name: 'Tarea 2', completada: false },
-        { name: 'Tarea 3', completada: true },
-    ]
 
-    const nuevaTarea = ref('')
+<script setup>
 
-    function agregarTarea() {
-        if (nuevaTarea.value.trim() !== '') {
-            items.push({ name: nuevaTarea.value, completada: false });
-            nuevaTarea.value = '';
-        }
+    function agregar() {
     }
-  </script>
+
+</script>
